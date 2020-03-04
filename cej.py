e3a5d98f3faa7f7df6f84d8fd2a7b15350aa3b52
telecorpse <saletrapotasowatosyf@gmail.com>
2020-03-04 20:45:22 +0100

    Prostszy filtr, nie wymaga dwóch tablic

--- cej.py
+++ cej.py
@@ -6,7 +6,7 @@ from PIL import Image
 strip_off = 500
 block_w = 20
 block_h = 20
-edge_thres = 200
+edge_thres = 100
 
 
 def norm255(array):
@@ -30,20 +30,20 @@ for f_path in sys.argv[1:]:
 	image = Image.open(f_path)
 	w, h = image.size
 	blocks = Np.zeros(w // block_w + 1, float)
-	edges = Np.zeros(len(blocks), float)
+	blocks = Np.zeros(len(blocks), float)
 	for i in range(strip_off, strip_off + block_h):
 		for j in range(0, w):
 			px = image.getpixel((j, i))
 			blocks[j // block_w] += max(px[0], max(px[1], px[2]))
 	for i in range(len(blocks)):
 		blocks[i] /= block_w * block_h
-	for i in range(1, len(blocks) - 1):
-		edges[i] = blocks[i-1] - blocks[i] * 2 + blocks[i+1]
-	norm255(edges)
-	thres255(edges, edge_thres)
+	for i in range(len(blocks) - 1):
+		blocks[i] = blocks[i] - blocks[i+1]
+	norm255(blocks)
+	thres255(blocks, edge_thres)
 	for i in range(strip_off, strip_off + block_h):
 		for j in range(0, w):
-			val = int(edges[j // block_w])
+			val = int(blocks[j // block_w])
 			image.putpixel((j, i), (val, val, val))
 	image.save("out/" + f_name)
 
