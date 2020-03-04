9984606f8b817bbeca0df0f851bef3ac04759397
telecorpse <saletrapotasowatosyf@gmail.com>
2020-03-04 18:59:34 +0100

    Po sprzątaniu 1

--- cej.py
+++ cej.py
@@ -11,21 +11,20 @@ for f_path in sys.argv[1:]:
 	print(f_path)
 	image = Image.open(f_path)
 	w, h = image.size
-	strip = image.crop((0, strip_off, w, strip_off + block_h))
 	blocks = Np.zeros(w // block_w + 1, int)
-	for i in range(0, block_h):
+	for i in range(strip_off, strip_off + block_h):
 		for j in range(0, w):
-			px = strip.getpixel((j, i))
+			px = image.getpixel((j, i))
 			blocks[j // block_w] += max(px[0], max(px[1], px[2]))
 	print(blocks)
 	for i in range(len(blocks)):
 		blocks[i] //= block_w * block_h
 	print(blocks)
-	for i in range(0, block_h):
+	for i in range(strip_off, strip_off + block_h):
 		for j in range(0, w):
 			val = blocks[j // block_w]
-			strip.putpixel((j, i), (val, val, val))
-	strip.save("out/strip_" + f_name)
+			image.putpixel((j, i), (val, val, val))
+	image.save("out/" + f_name)
 
 
 	
